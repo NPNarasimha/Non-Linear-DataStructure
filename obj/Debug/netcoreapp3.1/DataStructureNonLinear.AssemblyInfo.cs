95b263796c52f465b8b505d01e3c1448f74f6ef7
NP Naraimha <prakowsalya.kowsalya@gmail.com>
2025-02-10 14:15:34 +0530

    initial commit

--- obj/Debug/netcoreapp3.1/DataStructureNonLinear.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/DataStructureNonLinear.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructureNonLinear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0efec63a00117539e72517cb9d4ac999d7c7272")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructureNonLinear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructureNonLinear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
