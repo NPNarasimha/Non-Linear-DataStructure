a0efec63a00117539e72517cb9d4ac999d7c7272
NP Naraimha <prakowsalya.kowsalya@gmail.com>
2025-02-10 14:04:23 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataStructureNonLinear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Tree t = new Tree();
+            t.Insert(10);
+            t.Insert(20);
+            t.Insert(30);
+            t.Insert(40);
+            t.Insert(50);
+
+            t.Pre_order(t.returnRoot());
+            Console.WriteLine();
+            t.In_order(t.returnRoot());
+            Console.WriteLine(); 
+            t.Post_order(t.returnRoot());
+
+
+        }
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureNonLinear
+{
+    internal class TreeNode
+    {
+        internal int value;
+        public  TreeNode left;
+       public  TreeNode right;
+        
+    }
+
+    internal class Tree
+    {
+        internal TreeNode root;
+        public Tree(TreeNode root)
+        {
+            root = null;
+        }
+
+        public Tree()
+        {
+        }
+
+        public TreeNode returnRoot()
+        {
+            return root;
+        }
+        
+        public void Insert(int data)
+        {
+            TreeNode t = new TreeNode();
+            t.value = data;
+            if (root == null)
+            {
+                root = t;
+            }
+            else
+            {
+                TreeNode current = root;
+                TreeNode parent;
+                while (true)
+                {
+                    parent = current;
+                    if (data < current.value)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = t;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current=current.right;
+                        if(current == null)
+                        {
+                            parent.right = t;
+                            return;
+                        }
+                    }
+               
+                }
+
+            }
+
+           
+        }
+        public void Pre_order(TreeNode root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.value+" ");
+                Pre_order(root.left);
+                Pre_order(root.right);
+            }
+        }
+        public void In_order(TreeNode root)
+        {
+            if (root != null)
+            {
+                
+                Pre_order(root.left);
+                Console.WriteLine(root.value + " ");
+                Pre_order(root.right);
+            }
+        }
+        public void Post_order(TreeNode root)
+        {
+            if (root != null)
+            {
+
+                Pre_order(root.left);
+                Pre_order(root.right);
+                Console.WriteLine(root.value + " ");
+            }
+        }
+    }
+}
